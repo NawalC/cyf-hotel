fad481f129c1fade75b6280bb515507c770faa4e
Nawal <nawal.careem@yahoo.com>
2018-08-11 15:11:25 +0100

    added return result in jason

---
+++ server.js
@@ -0,0 +1,62 @@
+const SERVER_PORT = process.env.PORT || 8080;
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+// const bodyparser = require("body-parser");
+
+// const apiRouter = require("./api");
+
+const app = express();
+// const router = express.Router();
+
+const formidable = require("express-formidable");
+app.use(formidable());
+
+const dbFilename = 'data/hotel.sqlite';
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database(dbFilename);
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs"
+  })
+);
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+app.use(express.static("assets"));
+
+// app.use("/api", apiRouter);
+
+// // handle HTTP POST requests
+// app.use(bodyparser.json());
+
+app.get("/", function(req, res, next) {
+  res.render("home");
+});
+
+const customerCallback = function (req, res)
+ {
+  db.all("SELECT name, email, phone, address, city " +
+         "FROM customers", function (err, rows) {
+    // rows.forEach(function (row) {
+    //   console.log(row.name, row.email, row.phone,
+    //               row.address, row.city);
+    // });
+const myData = {
+  customers:rows
+};
+res.status(200).json(myData);
+  });
+};
+
+
+
+app.get("/customers", customerCallback)
+
+
+app.listen(SERVER_PORT, () => {
+  console.info(`Server started at http://localhost:${SERVER_PORT}`);
+});
